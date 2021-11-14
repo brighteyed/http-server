7c50e1bb21efec38350ffb7779bba655820dffc6
brighteyed <sergey.kondrikov@gmail.com>
2021-11-14 12:39:23 +0300

    Create handler for the server

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/brighteyed/http-server/config"
+	"github.com/brighteyed/http-server/server"
+	"github.com/brighteyed/http-server/tracker"
 )
 
 func main() {
@@ -11,4 +20,51 @@ func main() {
 	flag.Parse()
 
 	run(*root, *port, *idle)
+}
+
+func run(root string, port string, idleDuration uint) {
+	appCfg := config.NewAppConfig(root)
+	if appCfg == nil {
+		log.Fatal("Error loading application configuration")
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	defer func() {
+		signal.Stop(signalChan)
+	}()
+
+	idleTracker := tracker.NewIdleTracker(idleDuration)
+	idleConnsClosed := make(chan struct{})
+
+	srv := http.Server{
+		Addr:      ":" + port,
+		Handler:   server.NewHandler(appCfg.Locations),
+		ConnState: idleTracker.ConnState,
+	}
+
+	go func() {
+		select {
+		case <-idleTracker.Done():
+			shutdown(&srv)
+		case <-signalChan:
+			shutdown(&srv)
+		}
+
+		close(idleConnsClosed)
+	}()
+
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("ListenAndServer, %v", err)
+	}
+
+	// Wait until Shutdown returns
+	<-idleConnsClosed
+}
+
+func shutdown(srv *http.Server) {
+	log.Println("Shutting down")
+	if err := srv.Shutdown(context.Background()); err != nil {
+		log.Fatalf("Shutdown, %v", err)
+	}
 }
